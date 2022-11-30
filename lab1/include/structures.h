b4b6cf9b5f273ddcc334bc0caacb419e3c94e38a
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-11-30 20:09:23 +0300

    modified structures for data storing

--- lab1/include/structures.h
+++ lab1/include/structures.h
@@ -5,8 +5,6 @@
 #ifndef LOW_LEVEL_PROGRAMMING_STRUCTURES_H
 #define LOW_LEVEL_PROGRAMMING_STRUCTURES_H
 
-#endif //LOW_LEVEL_PROGRAMMING_STRUCTURES_H
-
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -16,22 +14,14 @@
 #define PATH_SIZE 260
 
 typedef struct {
-    char path[PATH_SIZE];
-    bool isDirectory;
-    void* patent;
-    void* children;
-    void* data_cell;
-} node;
-
-typedef struct {
     /*
-     * Field for storing data type. Size of this field is 2 bits, where
-     * 00 - boolean variables
-     * 01 - integer numbers
-     * 10 - numbers with floating points
-     * 11 - strings
+     * Field for storing data type, where
+     * 0 - boolean variables
+     * 1 - integer numbers
+     * 2 - numbers with floating points
+     * 3 - strings
      */
-    unsigned int data_type : DATA_TYPE_BIT_SIZE;
+    unsigned int data_type;
 } data_type;
 
 typedef struct {
@@ -40,10 +30,15 @@ typedef struct {
      * abstract info about data's blocks, where:
      */
     data_type data_type;                // root element for defining data type
+    char pathToThisNode[4096];          // unix-like path to this node in document tree
     uint64_t offset_of_current_block;   // hash for unambiguous definition of this data block
     uint64_t offset_of_previous_block;  // hash for unambiguous definition of previous part of this data block
     uint64_t offset_of_next_block;      // hash for unambiguous definition of continuation of this data block
-    // (for case when data have not been stored in one data block)
+                                        // (for case when data have not been stored in one data block)
+    uint64_t offset_to_parent_node;
+    uint64_t children[4294967295];      // array of offsets to children nodes - size of array like in NTFS3уы
+    bool isDirectory;
+
 } data;
 
 typedef struct {
@@ -71,7 +66,7 @@ typedef struct {
      * boolean data type, where:
      */
     data data;                       // abstract data block description
-    int data_cell : DATA_BIT_SIZE_FOR_BOOLEAN;  // cell for clear data, 1-bit number (0 == false, 1 == true)
+    bool data_cell : DATA_BIT_SIZE_FOR_BOOLEAN;  // cell for clear data, 1-bit number (0 == false, 1 == true)
 } boolean;
 
 typedef struct {
@@ -80,16 +75,18 @@ typedef struct {
      * strings, where:
      */
     data data;   // abstract data block description
-    char data_cell[];   // cell for clear data, char[] array
+    char data_cell[1024];   // cell for clear data, char[] array
 } string;
 
-typedef struct {
-    /*
-     * Info about request. 00 - create new block, 01 - read existing block
-     * 10 - update existing block, 11 - delete existing block
-     * Data for manipulation will be requested additionally
-     */
-    unsigned int request_type : REQUEST_BIT_SIZE;
-    char path[PATH_SIZE];
-    char user_data[]; // necessary for CREATE, UPDATE operations, empty for READ and DELETE
-} request;
+//typedef struct {
+//    /*
+//     * Info about request. 00 - create new block, 01 - read existing block
+//     * 10 - update existing block, 11 - delete existing block
+//     * Data for manipulation will be requested additionally
+//     */
+//    unsigned int request_type : REQUEST_BIT_SIZE;
+//    char path[PATH_SIZE];
+//    char user_data[]; // necessary for CREATE, UPDATE operations, empty for READ and DELETE
+//} request;
+
+#endif //LOW_LEVEL_PROGRAMMING_STRUCTURES_H
