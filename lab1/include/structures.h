690005c16bd16b0ea6f5018c1ab44c8c0533e2c9
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-12-06 14:06:51 +0300

    structs for data chunks reorganized to avoid problems with serializing

--- lab1/include/structures.h
+++ lab1/include/structures.h
@@ -14,7 +14,7 @@
 #define REQUEST_BIT_SIZE 2
 #define PATH_SIZE 260
 
-typedef struct {
+struct {
     /*
      * Field for storing data type, where
      * 0 - boolean variables
@@ -30,16 +30,22 @@ typedef struct {
      * Child element of root element in my document tree for storing
      * abstract info about data's blocks, where:
      */
-    data_type data_type;                // root element for defining data type
-    char pathToThisNode[4096];          // unix-like path to this node in document tree
+    /*
+     * Field for storing data type, where
+     * 0 - boolean variables
+     * 1 - integer numbers
+     * 2 - numbers with floating points
+     * 3 - strings
+     */
+    unsigned int data_type;
+    char pathToThisNode[1024];          // unix-like path to this node in document tree
     uint64_t offset_of_current_block;   // hash for unambiguous definition of this data block
     uint64_t offset_of_previous_block;  // hash for unambiguous definition of previous part of this data block
     uint64_t offset_of_next_block;      // hash for unambiguous definition of continuation of this data block
                                         // (for case when data have not been stored in one data block)
     uint64_t offset_to_parent_node;
-    uint64_t children[65536];           // array of offsets to children nodes - size of array like in NTFS
+    uint64_t children[10];           // array of offsets to children nodes
     bool isDirectory;
-
 } data;
 
 typedef struct {
@@ -47,36 +53,56 @@ typedef struct {
      * Child element of root element in my document tree for storing
      * integer numbers, where:
      */
-    data data;       // abstract data block description
+    unsigned int data_type;
+    char pathToThisNode[1024];          // unix-like path to this node in document tree
+    uint64_t offset_of_current_block;   // hash for unambiguous definition of this data block
+    uint64_t offset_of_previous_block;  // hash for unambiguous definition of previous part of this data block
+    uint64_t offset_of_next_block;      // hash for unambiguous definition of continuation of this data block
+    // (for case when data have not been stored in one data block)
+    uint64_t offset_to_parent_node;
+    uint64_t children[10];           // array of offsets to children nodes
+    bool isDirectory;
     int32_t data_cell;      // cell for clear data, 32-bit signed number (like in tech task).
     // May be extended via using hash to next data block (optional).
 } integer;
 
 typedef struct {
-    /*
-     * Child element of root element in my document tree for storing
-     * numbers with floating points, where:
-     */
-    data data;   // abstract data block description
-    double data_cell;   // cell for clear data, 64-bit signed floating numbers
+    unsigned int data_type;
+    char pathToThisNode[1024];          // unix-like path to this node in document tree
+    uint64_t offset_of_current_block;   // hash for unambiguous definition of this data block
+    uint64_t offset_of_previous_block;  // hash for unambiguous definition of previous part of this data block
+    uint64_t offset_of_next_block;      // hash for unambiguous definition of continuation of this data block
+    // (for case when data have not been stored in one data block)
+    uint64_t offset_to_parent_node;
+    uint64_t children[10];           // array of offsets to children nodes
+    bool isDirectory;
+    float data_cell;   // cell for clear data, 64-bit signed floating numbers
 } floating_number;
 
 typedef struct {
-    /*
-     * Child element of root element in my document tree for storing
-     * boolean data type, where:
-     */
-    data data;                       // abstract data block description
+    unsigned int data_type;
+    char pathToThisNode[1024];          // unix-like path to this node in document tree
+    uint64_t offset_of_current_block;   // hash for unambiguous definition of this data block
+    uint64_t offset_of_previous_block;  // hash for unambiguous definition of previous part of this data block
+    uint64_t offset_of_next_block;      // hash for unambiguous definition of continuation of this data block
+    // (for case when data have not been stored in one data block)
+    uint64_t offset_to_parent_node;
+    uint64_t children[10];           // array of offsets to children nodes
+    bool isDirectory;
     bool data_cell : DATA_BIT_SIZE_FOR_BOOLEAN;  // cell for clear data, 1-bit number (0 == false, 1 == true)
 } boolean;
 
 typedef struct {
-    /*
-     * Child element of root element in my document tree for storing
-     * strings, where:
-     */
-    data data;   // abstract data block description
-    char data_cell[1024];   // cell for clear data, char[] array
+    unsigned int data_type;
+    char pathToThisNode[1024];          // unix-like path to this node in document tree
+    uint64_t offset_of_current_block;   // hash for unambiguous definition of this data block
+    uint64_t offset_of_previous_block;  // hash for unambiguous definition of previous part of this data block
+    uint64_t offset_of_next_block;      // hash for unambiguous definition of continuation of this data block
+    // (for case when data have not been stored in one data block)
+    uint64_t offset_to_parent_node;
+    uint64_t children[10];           // array of offsets to children nodes
+    bool isDirectory;
+    char data_cell[4];   // cell for clear data, char[] array
 } string;
 
 enum type { my_int, my_float, my_boolean,  my_string };
