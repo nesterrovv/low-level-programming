1001b976c066336fa67679fce25a970eec5c9602
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-12-07 14:16:16 +0300

    added declarations of some functions for implemenation in nearest future

--- lab1/include/file_manager.h
+++ lab1/include/file_manager.h
@@ -5,9 +5,13 @@
 #ifndef LOW_LEVEL_PROGRAMMING_FILE_MANAGER_H
 #define LOW_LEVEL_PROGRAMMING_FILE_MANAGER_H
 
+long count_size_of_file(FILE* file);
+bool is_chunk_exists(FILE* file, int offset_of_chunk);
+int read_chunk_integer(long offset, integer* destination);
 int write_to_navigator(char pathToFile[], int offset);
 int clear_navigator();
 int download_navigator();
 int count_blocks();
+int write_to_data_heap(struct some_type* some_type, char* necessaryPath);
 
 #endif //LOW_LEVEL_PROGRAMMING_FILE_MANAGER_H
